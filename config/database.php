eb9eee020bbc7b7bcca91d1d248dd689b92c505d
Alejandro reyes <jorgerenteriareyes4@gmail.com>
2026-08-16 20:08:44 -0600

    se agrego funcinalidad manejar dos bases de datos replica y maestro

--- config/database.php
+++ config/database.php
@@ -48,11 +48,24 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'read' => [
+                'host' => [
+                    env('DB_HOST_READ', env('DB_HOST', '127.0.0.1')),
+                ],
+                'port' => env('DB_PORT_READ', env('DB_PORT', '3306')),
+                'username' => env('DB_USERNAME_READ', env('DB_USERNAME', 'root')),
+                'password' => env('DB_PASSWORD_READ', env('DB_PASSWORD', '')),
+            ],
+            'write' => [
+                'host' => [
+                    env('DB_HOST_WRITE', env('DB_HOST', '127.0.0.1')),
+                ],
+                'port' => env('DB_PORT_WRITE', env('DB_PORT', '3306')),
+                'username' => env('DB_USERNAME_WRITE', env('DB_USERNAME', 'root')),
+                'password' => env('DB_PASSWORD_WRITE', env('DB_PASSWORD', '')),
+            ],
+            'sticky' => true,
             'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
